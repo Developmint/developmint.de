ca5e5f96bdc08064502d69dbf64c25575c317216
Alexander Lichter <manniL@gmx.net>
2019-02-25 15:05:28 +0000

    fix: str

--- plugins/vue-observe-visibility-directive.client.js
+++ plugins/vue-observe-visibility-directive.client.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import { ObserveVisibility } from 'vue-observe-visibility'
 
 export default async function () {
-  if (!(IntersectionObserver in window)) {
+  if (!('IntersectionObserver' in window)) {
     await import('intersection-observer')
   }
   Vue.directive('observe-visibility', ObserveVisibility)
