e2e662908e486a93d951a062f97936581da8d010
kevin olson <acidjazz@gmail.com>
2019-03-19 04:56:35 -0500

    feat: add modal transition (#59)

--- components/ContactUsModal.vue
+++ components/ContactUsModal.vue
@@ -1,134 +1,136 @@
 <template>
-  <div class="fixed pin-t w-full h-screen bg-rains-trans z-20 overflow-hidden md:overflow-y-scroll text-white p-4 lg:p-8 lg:pt-0 xl:p-16 text-2xl lg:text-3xl transition-all">
-    <button
-      class="close-button"
-      @click="$emit('close')"
-    />
-    <div class="m-4 mt-0 lg:m-16 xl:m-32 mx-auto flex flex-col items-center justify-center">
-      <h2 class="text-white text-2xl md:text-3xl xl:text-5xl text-center lg:text-left capitalize font-normal mb-2 lg:mb-4 xl:mb-8">
-        {{ $t('contact.heading') }}
-      </h2>
-      <form
-        v-show="!isSubmitted"
-        class=" w-5/6 lg:w-2/3 xl:w-1/3 mx-auto flex flex-col items-center text-sm"
-        @submit.prevent="validate"
-      >
+  <transition name="animodal">
+    <div class="fixed pin-t w-full h-screen bg-rains-trans z-20 overflow-hidden md:overflow-y-scroll text-white p-4 lg:p-8 lg:pt-0 xl:p-16 text-2xl lg:text-3xl transition-all">
+      <button
+        class="close-button modal-close"
+        @click="$emit('close')"
+      />
+      <div class="m-4 mt-0 lg:m-16 xl:m-32 mx-auto flex flex-col items-center justify-center animodal-inner">
+        <h2 class="text-white text-2xl md:text-3xl xl:text-5xl text-center lg:text-left capitalize font-normal mb-2 lg:mb-4 xl:mb-8">
+          {{ $t('contact.heading') }}
+        </h2>
+        <form
+          v-show="!isSubmitted"
+          class=" w-5/6 lg:w-2/3 xl:w-1/3 mx-auto flex flex-col items-center text-sm"
+          @submit.prevent="validate"
+        >
+          <div
+            :class="{'shadow-white': focusedElement === 'name', 'shadow-red': $v.name.$error}"
+            class="bg-grey-lighter inline-flex rounded items-center px-4 border-white transition-all w-full my-2"
+          >
+            <label
+              for="name"
+              class="text-developmint-darker whitespace-no-wrap"
+            >
+              Name
+            </label>
+            <input
+              id="name"
+              v-model.trim="$v.name.$model"
+              class="appearance-none bg-transparent py-4 px-2 mx-2 md:mx-0 md:px-8 w-5/6 md:w-full text-developmint-darkest"
+              type="text"
+              @focus="focusedElement = 'name'"
+              @blur="focusedElement = ''"
+            >
+          </div>
+          <span
+            v-show="$v.name.$error"
+            class="text-sm self-start ml-2 mb-4 text-red-light"
+          >
+            Please type a valid name
+          </span>
+          <div
+            :class="{'shadow-white': focusedElement === 'email', 'shadow-red': $v.email.$error}"
+            class="bg-grey-lighter inline-flex rounded items-center px-4 border-white transition-all w-full my-2"
+          >
+            <label
+              for="email"
+              class="text-developmint-darker whitespace-no-wrap"
+            >
+              E-Mail
+            </label>
+            <input
+              id="email"
+              v-model.trim="$v.email.$model"
+              class="appearance-none bg-transparent py-4 px-2 mx-2 md:mx-0 md:px-8 w-5/6 md:w-full text-developmint-darkest"
+              type="email"
+              @focus="focusedElement = 'email'"
+              @blur="focusedElement = ''"
+            >
+          </div>
+          <span
+            v-show="$v.email.$error"
+            class="text-sm self-start ml-2 mb-4 text-red-light"
+          >
+            Please insert a valid E-Mail
+          </span>
+          <div
+            :class="{'shadow-white': focusedElement === 'msg', 'shadow-red': $v.msg.$error}"
+            class="bg-grey-lighter inline-flex flex-col rounded px-4 border-white transition-all w-full my-2"
+          >
+            <label
+              for="msg"
+              class="py-4 text-developmint-darker"
+            >
+              {{ $t('contact.fields.tell') }}
+            </label>
+            <textarea
+              id="msg"
+              v-model.trim="$v.msg.$model"
+              class="appearance-none bg-transparent text-developmint-darkest resize-none"
+              rows="6"
+              @focus="focusedElement='msg'"
+              @blur="focusedElement = ''"
+            />
+          </div>
+          <span
+            v-show="$v.msg.$error"
+            class="text-xm self-start ml-2 mb-4 text-red-light"
+          >
+            Please provide a valid message with at least 25 characters
+          </span>
+          <p class="text-xs md:text-base my-4 px-2 text-grey-light">
+            We use the data you provide us through this form only to process your contact request! You
+            find more information in our
+            <NuxtLink
+              :to="localePath('privacy')"
+              tabindex="-1"
+              class="text-white hover:text-developmint-lighter"
+            >
+              Privacy Policy
+            </NuxtLink>
+          </p>
+          <div class="flex justify-between lg:block lg:ml-auto">
+            <button
+              class="lg:hidden mr-4 mt-4 rounded hover:border-yellow transition-all hover:text-yellow px-6 py-3 border border-yellow-dark text-grey-light"
+              @click.prevent="$emit('close')"
+            >
+              {{ $t('contact.buttons.back') }}
+            </button>
+            <button
+              :class="submitButtonClasses"
+              :disabled="submissionDisabled"
+              type="submit"
+            >
+              {{ $t('contact.buttons.submit') }}
+            </button>
+          </div>
+        </form>
         <div
-          :class="{'shadow-white': focusedElement === 'name', 'shadow-red': $v.name.$error}"
-          class="bg-grey-lighter inline-flex rounded items-center px-4 border-white transition-all w-full my-2"
+          v-show="isSubmitted"
+          class="text-2xl text-developmint"
         >
-          <label
-            for="name"
-            class="text-developmint-darker whitespace-no-wrap"
-          >
-            Name
-          </label>
-          <input
-            id="name"
-            v-model.trim="$v.name.$model"
-            class="appearance-none bg-transparent py-4 px-2 mx-2 md:mx-0 md:px-8 w-5/6 md:w-full text-developmint-darkest"
-            type="text"
-            @focus="focusedElement = 'name'"
-            @blur="focusedElement = ''"
-          >
+          Thanks for your submission!
         </div>
-        <span
-          v-show="$v.name.$error"
-          class="text-sm self-start ml-2 mb-4 text-red-light"
-        >
-          Please type a valid name
-        </span>
         <div
-          :class="{'shadow-white': focusedElement === 'email', 'shadow-red': $v.email.$error}"
-          class="bg-grey-lighter inline-flex rounded items-center px-4 border-white transition-all w-full my-2"
+          v-if="error"
+          class="text-2xl text-red-dark"
         >
-          <label
-            for="email"
-            class="text-developmint-darker whitespace-no-wrap"
-          >
-            E-Mail
-          </label>
-          <input
-            id="email"
-            v-model.trim="$v.email.$model"
-            class="appearance-none bg-transparent py-4 px-2 mx-2 md:mx-0 md:px-8 w-5/6 md:w-full text-developmint-darkest"
-            type="email"
-            @focus="focusedElement = 'email'"
-            @blur="focusedElement = ''"
-          >
+          An error occurred!
         </div>
-        <span
-          v-show="$v.email.$error"
-          class="text-sm self-start ml-2 mb-4 text-red-light"
-        >
-          Please insert a valid E-Mail
-        </span>
-        <div
-          :class="{'shadow-white': focusedElement === 'msg', 'shadow-red': $v.msg.$error}"
-          class="bg-grey-lighter inline-flex flex-col rounded px-4 border-white transition-all w-full my-2"
-        >
-          <label
-            for="msg"
-            class="py-4 text-developmint-darker"
-          >
-            {{ $t('contact.fields.tell') }}
-          </label>
-          <textarea
-            id="msg"
-            v-model.trim="$v.msg.$model"
-            class="appearance-none bg-transparent text-developmint-darkest resize-none"
-            rows="6"
-            @focus="focusedElement='msg'"
-            @blur="focusedElement = ''"
-          />
-        </div>
-        <span
-          v-show="$v.msg.$error"
-          class="text-xm self-start ml-2 mb-4 text-red-light"
-        >
-          Please provide a valid message with at least 25 characters
-        </span>
-        <p class="text-xs md:text-base my-4 px-2 text-grey-light">
-          We use the data you provide us through this form only to process your contact request! You
-          find more information in our
-          <NuxtLink
-            :to="localePath('privacy')"
-            tabindex="-1"
-            class="text-white hover:text-developmint-lighter"
-          >
-            Privacy Policy
-          </NuxtLink>
-        </p>
-        <div class="flex justify-between lg:block lg:ml-auto">
-          <button
-            class="lg:hidden mr-4 mt-4 rounded hover:border-yellow transition-all hover:text-yellow px-6 py-3 border border-yellow-dark text-grey-light"
-            @click.prevent="$emit('close')"
-          >
-            {{ $t('contact.buttons.back') }}
-          </button>
-          <button
-            :class="submitButtonClasses"
-            :disabled="submissionDisabled"
-            type="submit"
-          >
-            {{ $t('contact.buttons.submit') }}
-          </button>
-        </div>
-      </form>
-      <div
-        v-show="isSubmitted"
-        class="text-2xl text-developmint"
-      >
-        Thanks for your submission!
-      </div>
-      <div
-        v-if="error"
-        class="text-2xl text-red-dark"
-      >
-        An error occurred!
       </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
