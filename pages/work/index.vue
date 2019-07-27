820f90b430f23773adc085eb1ddd8d8cf00dc931
Alexander Lichter <manniL@gmx.net>
2019-07-27 22:21:28 +0200

    style: improve mobile spacing for animated numbers

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -28,7 +28,7 @@
           v-for="({duration, to, prefix, precision}, i) in $options.numbers"
           :key="i"
           v-observe-visibility="{ callback: !i ? handleVisibility : () => {}, once: true }"
-          class="my-6 md:my-8 lg:my-0 mx-4 text-xl text-center w-1/3"
+          class="my-6 md:my-8 lg:my-0 mx-4 text-xl text-center md:w-1/3"
         >
           <AnimatedNumber
             :duration="duration"
