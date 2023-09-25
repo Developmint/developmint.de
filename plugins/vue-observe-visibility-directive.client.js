3c1daf26de9acc61b79b411ad1d208076e8919c0
Alexander Lichter <github@lichter.io>
2023-09-25 16:43:44 +0200

    chore: redirect to lichter.io (#121)
    👋

--- plugins/vue-observe-visibility-directive.client.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import { ObserveVisibility } from 'vue-observe-visibility'
-
-export default async function () {
-  if (!('IntersectionObserver' in window)) {
-    await import('intersection-observer')
-  }
-  Vue.directive('observe-visibility', ObserveVisibility)
-}
